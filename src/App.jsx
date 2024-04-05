b8a20ad0766ddc6288193bda52e775648c92fd08
SukhovetskaTetiana <garmonia.t@gmail.com>
2024-04-05 14:47:12 +0200

    added for task-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import NewProject from "./components/NewProject.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <NewProject />
     </main>
   );
 }
