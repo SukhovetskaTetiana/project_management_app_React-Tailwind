51c9d988de8a8c6966ae235971c79e0f7b3b702a
SukhovetskaTetiana <garmonia.t@gmail.com>
2024-04-06 11:14:14 +0200

    create Button App amd did some state switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import React from "react";
+import { useState } from "react";
+
 import NewProject from "./components/NewProject.jsx";
+import NoSelectProjects from "./components/NoProjectSelected.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  const handleStartAddProject = () => {
+    console.log("handleStartAddProject in App work!!");
+
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoSelectProjects onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NewProject />
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,17 @@
-export default function Sidebar() {
+import Button from "./Button";
+
+export default function Sidebar({ onStartAddProject }) {
   const asideClasses =
-    "bg-brown-950 text-white rounded-r-3xl px-12 py-20 md:px-14  lg:px-19 space-y-10 text-left md:shrink-0 w-1/2 md:w-2/5 lg:w-1.5/3";
+    "bg-brown-950 text-white rounded-r-3xl px-12 py-20 md:px-14  lg:px-19 space-y-10 text-left md:shrink-0 min-w-[300px]";
 
-  const h2Classes =
-    "text-brown-200 text-xl md:text-2xl lg:text-4xl font-bold uppercase";
+  const h2Classes = "text-brown-200 text-xl font-bold uppercase";
 
-  const buttonClasses =
-    "px-4 py-2 text-lg md:text-xl lg:text-3xl text-brown-400 bg-brown-900 rounded-lg hover:text-brown-900 hover:bg-brown-400 focus:outline-none focus:ring focus:ring-brown-600";
-
-  const ulClasses = "text-brown-200 space-y-2 text-md md:text-2xl";
+  const ulClasses = "text-brown-200 space-y-2 text-md";
 
   return (
     <aside className={asideClasses}>
       <h2 className={h2Classes}>your projects</h2>
-      <button className={buttonClasses}>+ Add Project</button>
+      <Button onClick={onStartAddProject}>+ Add Project</Button>
       <ul className={ulClasses}>
         <li>Project 1</li>
         <li>Project 2</li>
