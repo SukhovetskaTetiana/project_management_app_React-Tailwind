8d1f3419365626a991c612287d22b81224f5a4b0
SukhovetskaTetiana <garmonia.t@gmail.com>
2024-04-16 14:49:42 +0200

    deploy app

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/project_management_app_React-Tailwind",
+  base: "/project_management_app_React-Tailwind/",
   plugins: [react()],
 });
